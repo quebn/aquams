7b825a461e55b1eebb6f0055eb77724be3b12424
Quebn <gonzalesquebin@gmail.com>
2024-06-12 00:01:02 +0800

    exploring navigation bar creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+// TODO: List UI
+// Create a list view and when selecting any of the list,
+// another date of selected list should show.
+
 void main() {
   runApp(const App());
 }
@@ -10,10 +14,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "AquaMS",
-      home: const HomePage(),
+      home: const Navigation(),
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
       ),
     );
@@ -22,29 +24,136 @@ class App extends StatelessWidget {
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
-  State<HomePage> createState() => HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class HomePageState extends State<HomePage> {
+// TODO:
+// [*] Create Navigation.
+// [ ] Clean and create simple container for a list component.
+// [ ] Add new item to list view with popup to define the value.
+// [ ] Make created list persist.
+// [ ] Add card popup to view to edit the list.
+
+class _HomePageState extends State<HomePage> {
+
+  List<String> todo = <String>[];
+  final List<String> entries = <String>["A", "B", "C"];
+  final List<int> colorCodes = <int>[600, 300, 100];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        flexibleSpace: const Text("Leading"),
-        title: const Text("Home"),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        //title: const Text("Home"),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
-        ),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(2),
+        itemCount: todo.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 50,
+            color: Colors.amber[600],
+            child: Center(child: Text("Entry ${todo[index]}"))
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
       ),
+    );
+  }
+}
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int currentPageIndex = 0;
+  List<Widget> navbarNavigations = <Widget>[
+    const NavigationDestination(
+      selectedIcon: Icon(Icons.home),
+      icon: Icon(Icons.home_outlined),
+      label: "Home",
+    ),
+    const NavigationDestination(
+      icon: Badge(child: Icon(Icons.notifications_sharp)),
+      label: "Notifications",
+    ),
+    const NavigationDestination(
+      icon: Badge(
+        label: Text("2"),
+        child: Icon(Icons.messenger_sharp),
+      ),
+      label: "Messages",
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context){
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index){
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: navbarNavigations,
+      ),
+      body: <Widget>[
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                "Home Page",
+                style: theme.textTheme.titleLarge,
+              ),
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text("Notifications 1"),
+                  subtitle: Text("This is a notification"),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text("Notifications 2"),
+                  subtitle: Text("This is a another notification"),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                "Home Page",
+                style: theme.textTheme.titleLarge,
+              ),
+            ),
+          ),
+        ),
+      ][currentPageIndex],
     );
   }
 }
